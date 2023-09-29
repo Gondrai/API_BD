ff4a9a30c7557411e1ed1fba1dcdd77f43772364
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2023-09-29 00:02:53 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+headers = {'charset': 'utf-8'}
+from flask import Flask, request, jsonify
+from database_functions import insert_atores, select_atores, delete_atores, update_atores, insert_filme
+
+app = Flask(__name__)
+
+@app.post("/api/Inserir atores")
+def insert_ator_route():
+    data = request.get_json()
+    primeiro_nome = data["primeiro_nome"]
+    sobrenome = data["sobrenome"]
+    data_nasc = data["data_nasc"]
+    premios_id = data["premios_id"]
+    id_ator = insert_atores(primeiro_nome, sobrenome, data_nasc, premios_id)
+    return {"id": id_ator, "message": f"Ator(a) {primeiro_nome} inserido!"}
+
+@app.get('/api/Consultar atores')
+def consultar_atores_route():
+    try:
+        data = request.get_json()
+        sobrenome = data["sobrenome"]
+        primeiro_nome = data["primeiro_nome"]
+        resultados = select_atores(sobrenome, primeiro_nome)
+        return jsonify(resultados)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.get('/api/deletar atores')
+def deletar_atores_route():
+    data = request.get_json()
+    sobrenome = data["sobrenome"]
+    message = delete_atores(sobrenome)
+    return {"message": message}
+
+@app.post('/api/Atualizar atores')
+def deletar_atores_route():
+    data = request.get_json()
+    sobrenome = data["sobrenome"]
+    primeiro_nome = data["primeiro_nome"]
+    message = update_atores(sobrenome, primeiro_nome)
+    return {"message": message}
+
+@app.post('/api/Inserir Filmes')
+def inserir_filmes_route():
+    data = request.get_json()
+    idioma_original = data["idioma_original"]
+    titulo = data["titulo"]
+    subtitulo = data["subtitulo"]
+    sinopse = data["sinopse"]
+    ano = data["ano"]
+    duracao = data["duracao"]
+    id_premios = data["id_premios"]
+    id_roteirista = data["id_roteirista"]
+    id_diretor = data["id_roteirista"]
+    id_filme = insert_filme(idioma_original, titulo, subtitulo, sinopse, ano, duracao, id_premios, id_roteirista, id_diretor)
+    return {"id": id_filme, "message": f"Ator(a) {titulo} inserido!"}
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ database_functions.py
@@ -0,0 +1,90 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+INSERT_ATOR = (
+    "INSERT INTO sistema.ator(primeiro_nome, sobrenome, data_nasc, id_premios) VALUES (%s,%s,%s,%s)"
+) # Inserir ator 
+
+SELECT_ATOR = (
+    "SELECT primeiro_nome, sobrenome, data_nasc, id_premios FROM sistema.ator WHERE sobrenome LIKE %s" 
+) # Selecionar ator 
+
+DELETE_ATOR = (
+    "DELETE FROM sistema.ator WHERE sobrenome ILIKE %s and primeiro_nome = %s "
+) # Deletar ator 
+
+UPDATE_ATOR = (
+    "UPDATE sistema.ator SET primeiro_nome = %s , sobrenome = %s , data_nasc = %s , id_premios =  %s WHERE primeiro_nome = %s and sobrenome = %s"
+) # Atualizar ator 
+
+INSERT_FILME = (
+    "INSERT INTO sistema.filme (idioma_original, titulo, subtitulo, sinopse, ano, duracao, id_premios, id_roteirista, id_diretor) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+) # Inserir filme 
+
+SELECT_FILME = (
+    "SELECT idioma_original, titulo, subtitulo, sinopse, ano, duracao, id_premios FROM sistema.filme WHERE titulo LIKE %s" 
+) # Selecionar filme 
+
+DELETE_FILME = (
+    "DELETE FROM sistema.filme WHERE titulo ILIKE %s and subtitulo ILIKE %s "
+) # Deletar filme 
+
+
+UPDATE_FILME = (
+    "UPDATE sistema.filme SET titulo='%s', subtitulo='%s', idioma_original='%s', sinopse='%s, ano='%s', duracao='%s' WHERE titulo ILIKE '%s'"
+) # Atualizar filme 
+
+load_dotenv()
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+def insert_atores(primeiro_nome, sobrenome, data_nasc, id_premios):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_ATOR, (primeiro_nome, sobrenome, data_nasc,id_premios))
+            ator_id = cursor.fetchone()[0]
+    return ator_id
+
+def select_atores(sobrenome):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ATOR, (f'%{sobrenome}%',))
+            resultados = cursor.fetchall()
+        
+        resultado_json = []
+        for resultado in resultados:
+            resultado_json.append({
+                'primeiro_nome': resultado[0],
+                'sobrenome': resultado[1],
+                'data_nasc': resultado[2],
+                'id_premios': resultado[3]
+            })
+
+        return resultado_json
+
+def delete_atores(sobrenome, primeiro_nome):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(DELETE_ATOR, (sobrenome, primeiro_nome))
+    return f"Ator(a) {sobrenome} Deletado!"
+
+
+def update_atores(primeiro_nome, sobrenome, data_nasc, id_premios):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_ATOR, (primeiro_nome, sobrenome, data_nasc, id_premios))
+            ator_id = cursor.fetchone()[0]
+    return ator_id
+
+
+def insert_filme(idioma_original, titulo, subtitulo, sinopse, ano, duracao, id_premios, id_roteirista, id_diretor):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_ATOR, (idioma_original, titulo, subtitulo, sinopse, ano, duracao, id_premios, id_roteirista, id_diretor))
+            ator_id = cursor.fetchone()[0]
+    return ator_id
+
+
+
+
